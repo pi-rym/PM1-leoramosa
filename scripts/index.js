947374ef427698e615e385d8a00498fed6539704
leoramosa <leo.ramos.ari@gmail.com>
2024-03-14 04:25:15 -0300

    homework 2

---
+++ scripts/index.js
@@ -0,0 +1,40 @@
+class Activity {
+  constructor(id, title, description, imgUrl) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.imgUrl = imgUrl;
+  }
+}
+
+class Repository {
+  constructor() {
+    this.activities = [];
+    this.id = 0;
+  }
+
+  //un metodo que reciba datos de una actividad, cree una actividad nueva y la guarde en su array
+  getAllActivities() {
+    return this.activities;
+  }
+
+  // un metodo que le permita retornar todas las actividades
+  createActivity(title, description, imgUrl) {
+    const id = this.id++;
+    //TODO CREAMOS UNA INSTANCIA
+    const activity = new Activity(id, title, description, imgUrl);
+    this.activities.push(activity);
+  }
+
+  // un metodo que le permita filtrar las activdades
+
+  //eliminar activities
+  deleteActivity() {}
+}
+
+const RepositoryLeo = new Repository();
+//Prueba de push
+RepositoryLeo.createActivity("Leo", "ramos", "imgleo");
+console.log(RepositoryLeo);
+RepositoryLeo.createActivity("martin", "ramos", "imgleo");
+console.log(RepositoryLeo);
