17121722a382d67c72c9ce1b2630338acb7139b8
leoramosa <leo.ramos.ari@gmail.com>
2024-03-18 04:32:23 -0300

    homeword3 finalizado, busque otra manera para poder eliminar el card

--- scripts/index.js
+++ scripts/index.js
@@ -36,14 +36,31 @@ class Repository {
   }
 }
 
-const RepositoryLeo = new Repository();
-//Prueba de push
-RepositoryLeo.createActivity("Leo", "ramos", "imgleo");
-console.log(RepositoryLeo);
-RepositoryLeo.createActivity("martin", "ramos", "imgleo");
-console.log(RepositoryLeo);
-RepositoryLeo.deleteActivity(0);
-console.log(RepositoryLeo);
+// const RepositoryLeo = new Repository();
+// //Prueba de push
+// RepositoryLeo.createActivity("Leo", "ramos", "imgleo");
+// console.log(RepositoryLeo);
+// RepositoryLeo.createActivity("martin", "ramos", "imgleo");
+// console.log(RepositoryLeo);
+// RepositoryLeo.deleteActivity(0);
+// console.log(RepositoryLeo);
+
+const repository = new Repository();
+
+//funcion button delete
+// function handlerButtonCardDelete(event) {
+//   const deleteButtonId = event.target.id;
+//   const activityId = deleteButtonId;
+//   repository.deleteActivity(activityId);
+//   allActivities();
+// }
+
+function handlerButtonCardDelete(event) {
+  const containerCard = event.target.closest(".card_activity");
+  const activityId = containerCard.id;
+  repository.deleteActivity(parseInt(activityId));
+  allActivities();
+}
 
 function cardActivity(activity) {
   const { id, title, description, imgUrl } = activity;
@@ -59,10 +76,29 @@ function cardActivity(activity) {
   cardImg.src = imgUrl;
   cardImg.className = "card_img";
 
-  const containerCard = document.createElement("div");
-  containerCard.appendChild(contentCard);
-  containerCard.className = "card_activity";
-  containerCard.id = id;
+  const editButton = document.createElement("button");
+  editButton.innerHTML = "Edit";
+  editButton.className = "edit";
+
+  const deleteButton = document.createElement("button");
+  deleteButton.innerHTML = "Delete";
+  deleteButton.className = "delete";
+  deleteButton.id = id;
+  deleteButton.addEventListener("click", handlerButtonCardDelete);
+
+  const contentCardButton = document.createElement("div");
+  contentCardButton.className = "card_button";
+  contentCardButton.appendChild(editButton);
+  contentCardButton.appendChild(deleteButton);
+
+  const contentCardImg = document.createElement("div");
+  contentCardImg.appendChild(cardImg);
+  contentCardImg.className = "card_img";
+
+  const contentCardInfo = document.createElement("div");
+  contentCardInfo.appendChild(cardTitle);
+  contentCardInfo.appendChild(cardDescription);
+  contentCardInfo.className = "card_inf";
 
   const contentCard = document.createElement("div");
   contentCard.appendChild(contentCardInfo);
@@ -70,23 +106,51 @@ function cardActivity(activity) {
   contentCard.appendChild(contentCardButton);
   contentCard.className = "card_content";
 
-  const contentCardInfo = document.createElement("div");
-  contentCardInfo.appendChild(cardTitle);
-  contentCardInfo.appendChild(cardDescription);
-  contentCardInfo.className = "card_inf";
+  const containerCard = document.createElement("div");
+  containerCard.appendChild(contentCard);
+  containerCard.className = "card_activity";
+  containerCard.id = id;
 
-  const contentCardImg = document.createElement("div");
-  contentCardImg.appendChild(cardImg);
-  contentCardImg.className = "card_img";
-
-  const contentCardButton = document.createElement("div");
-  contentCardButton.className = "card_button";
   return containerCard;
 }
+
+//recorremos en reposity y lo convertimos en html
 
 function allActivities() {
   const containerActivities = document.getElementById("container_activities");
   containerActivities.innerHTML = "";
 
-  const activities = Repository.getAllActivities();
+  const activities = repository.getAllActivities();
+  const htmlActivities = activities.map((activity) => cardActivity(activity));
+
+  htmlActivities.forEach((htmlCard) => {
+    containerActivities.appendChild(htmlCard);
+  });
 }
+
+//creamos la funcion buuton para agregar info
+function handlerButtonCard(event) {
+  event.preventDefault();
+  const inputTitle = document.getElementById("input_title");
+  const inputDescription = document.getElementById("area_description");
+  const inputImg = document.getElementById("input_img");
+
+  const titleValue = inputTitle.value;
+  const areaValue = inputDescription.value;
+  const imgValue = inputImg.value;
+
+  if (!titleValue || !areaValue || !imgValue) {
+    alert("Por favor, todos los campos son obligatorios");
+  } else {
+    repository.createActivity(titleValue, areaValue, imgValue);
+    allActivities();
+    inputTitle.value = "";
+    inputDescription.value = "";
+    inputImg.value = "";
+  }
+}
+
+//creamos el boton para pushear
+
+const cardButton = document.getElementById("add_button");
+cardButton.addEventListener("click", handlerButtonCard);
